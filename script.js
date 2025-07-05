a12151b4b60a5ec2fce72618e8028e0803040ce0
phychodude <120724658+phychodude@users.noreply.github.com>
2025-07-05 21:29:03 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function copyIP() {
-    const ip = document.getElementById("server-ip").innerText;
-    navigator.clipboard.writeText(ip).then(() => {
-        alert("Server IP copied: " + ip);
-    });
-}
